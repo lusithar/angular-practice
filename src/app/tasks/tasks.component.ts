942da7f5982afd81f3d2fb3508489afec53e0807
lusithar <lusithar@ccl.lk>
2025-05-10 23:51:52 +0530

    Implemented the add new task behaviour

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -12,6 +13,7 @@ export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name?: string;
 
+  isAddingTask: boolean = false;
   dummyTasks = [
     {
       id: 't1',
@@ -44,5 +46,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.dummyTasks = this.dummyTasks.filter((dummyTask) => dummyTask.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
